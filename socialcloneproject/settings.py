13c9f13d24b65cb95c56a94d8fe2c8f32d54c2e5
Hossam <hossamhsn74@gmail.com>
2020-04-10 12:29:48 +0200

    posts and groups basic files created

--- socialcloneproject/settings.py
+++ socialcloneproject/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
+    'posts.apps.PostsConfig',
+    'groups.apps.GroupsConfig',
     'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
